476bb6539822cbf8f3ef1e2e41450a0e63c09e11
ZHU Zikun <75139323+zikunz@users.noreply.github.com>
2021-05-26 02:03:28 +0800

    sortedSquaredArray

---
+++ sortedSquaredArray/sortedSquaredArray.py
@@ -0,0 +1,22 @@
+# Time Complexity: O(N), where N represents number of elements in array
+# Space Complexity: O(N), where N represents number of elements in array
+
+
+def sortedSquaredArray(array):
+    sortedSquaredArray = [0 for _ in array]
+
+    smallerValueIdx = 0
+    largerValueIdx = len(array) - 1
+    sortedSquaredArrayIdx = len(array) - 1
+
+    while smallerValueIdx <= largerValueIdx:
+        if array[smallerValueIdx] ** 2 >= array[largerValueIdx] ** 2:
+            sortedSquaredArray[sortedSquaredArrayIdx] = array[smallerValueIdx] ** 2
+            sortedSquaredArrayIdx -= 1
+            smallerValueIdx += 1
+        else:
+            sortedSquaredArray[sortedSquaredArrayIdx] = array[largerValueIdx] ** 2
+            sortedSquaredArrayIdx -= 1
+            largerValueIdx -= 1
+
+    return sortedSquaredArray
