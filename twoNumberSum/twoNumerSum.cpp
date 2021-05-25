18c05faa96edb1ba04549d2d864da58719cc77ff
ZHU Zikun <75139323+zikunz@users.noreply.github.com>
2021-05-25 20:12:05 +0800

    Update twoNumerSum.cpp

--- twoNumberSum/twoNumerSum.cpp
+++ twoNumberSum/twoNumerSum.cpp
@@ -1,24 +1,18 @@
 #include <unordered_set>
-
 #include <vector>
-
 using namespace std;
 
-vector<int> twoNumberSum(vector<int> array, int targetSum)
-{
+vector<int> twoNumberSum(vector<int> array, int targetSum) {
     unordered_set<int> hash_table;
     vector<int> answer;
 
-    for (auto i = array.begin(); i != array.end(); ++i)
-    {
+    for (auto i = array.begin(); i != array.end(); ++i) {
         hash_table.insert(*i);
     }
 
-    for (auto i = array.begin(); i != array.end(); ++i)
-    {
+    for (auto i = array.begin(); i != array.end(); ++i) {
         auto got = hash_table.find((targetSum - *i));
-        if (got != hash_table.end() && (*i != *got))
-        {
+        if (got != hash_table.end() && (*i != *got)) {
             answer.push_back(*i);
             answer.push_back(*got);
             return answer;
