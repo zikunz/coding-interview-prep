080230453adc8414279fc3c667ec3817ded5b851
ZHU Zikun <75139323+zikunz@users.noreply.github.com>
2021-05-27 21:52:39 +0800

    removeDuplicatesFromLinkedList

---
+++ removeDuplicatesFromLinkedList/removeDuplicatesFromLinkedList.py
@@ -0,0 +1,21 @@
+# Time Complexity: O(N), where N represents number of elements in linkedList
+# Space Complexity: O(1)
+
+
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def removeDuplicatesFromLinkedList(linkedList):
+    currNode = linkedList
+
+    while currNode is not None:
+        nextDiffNode = currNode.next
+        while nextDiffNode is not None and nextDiffNode.value == currNode.value:
+            nextDiffNode = nextDiffNode.next
+        currNode.next = nextDiffNode
+        currNode = nextDiffNode
+
+    return linkedList
