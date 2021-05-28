786b07d5053b8b7bad5a16719c3cdaded017a982
ZHU Zikun <75139323+zikunz@users.noreply.github.com>
2021-05-28 23:54:29 +0800

    Add files via upload

---
+++ binarySearch/binarySearch.py
@@ -0,0 +1,18 @@
+# Time Comeplexity: O(logN), where N represents number of elements in array
+# Space Complexity: O(1)
+
+
+def binarySearch(array, target):
+    leftPtr = 0
+    rightPtr = len(array) - 1
+
+    while leftPtr <= rightPtr:
+        middlePtr = (leftPtr + rightPtr) // 2
+        if array[middlePtr] == target:
+            return middlePtr
+        elif array[middlePtr] > target:
+            rightPtr = middlePtr - 1
+        else:
+            leftPtr = middlePtr + 1
+
+    return -1
