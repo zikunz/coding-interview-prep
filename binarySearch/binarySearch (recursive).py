679fafd438dd6a5f71fbe6652969fb1207659bd5
ZHU Zikun <75139323+zikunz@users.noreply.github.com>
2021-05-29 00:35:26 +0800

    Add files via upload

---
+++ binarySearch/binarySearch (recursive).py
@@ -0,0 +1,20 @@
+# Time Complexity: O(logN), where N represents number of elements in array
+# Space Complexity: O(logN) (adding frames to the call stack due to recursive calls uses extra memory)
+
+
+def binarySearch(array, target):
+    binarySearchHelper(array, target, 0, len(array) - 1)
+
+
+def binarySearchHelper(array, target, leftPtr, rightPtr):
+    if leftPtr > rightPtr:
+        return -1
+
+    middlePtr = (leftPtr + rightPtr) // 2
+
+    if array[middlePtr] == target:
+        return middlePtr
+    elif array[middlePtr] > target:
+        return binarySearchHelper(array, target, leftPtr, middlePtr - 1)
+    else:
+        return binarySearchHelper(array, target, middlePtr + 1, rightPtr)
