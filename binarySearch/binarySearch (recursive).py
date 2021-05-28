078439063ba05dd3f071dcdc1365bd174199405a
ZHU Zikun <75139323+zikunz@users.noreply.github.com>
2021-05-29 00:36:39 +0800

    Update binarySearch (recursive).py

--- binarySearch/binarySearch (recursive).py
+++ binarySearch/binarySearch (recursive).py
@@ -1,10 +1,8 @@
 # Time Complexity: O(logN), where N represents number of elements in array
 # Space Complexity: O(logN) (adding frames to the call stack due to recursive calls uses extra memory)
 
-
 def binarySearch(array, target):
-    binarySearchHelper(array, target, 0, len(array) - 1)
-
+    return binarySearchHelper(array, target, 0, len(array) - 1)
 
 def binarySearchHelper(array, target, leftPtr, rightPtr):
     if leftPtr > rightPtr:
