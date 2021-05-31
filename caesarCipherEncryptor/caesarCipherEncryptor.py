4173df0c6ef4382e9ad26e0cb47211e0be6fb7cd
ZHU Zikun <75139323+zikunz@users.noreply.github.com>
2021-05-31 21:14:18 +0800

    Update caesarCipherEncryptor.py

--- caesarCipherEncryptor/caesarCipherEncryptor.py
+++ caesarCipherEncryptor/caesarCipherEncryptor.py
@@ -1,14 +1,15 @@
 # Time Complexity: O(N), where N represents number of elements in string
 # Space Complexity: O(N), where N represents number of elements in string
+
 def caesarCipherEncryptor(string, key):
     newLetters = []
 	
-	for letter in string:
-		nLC = ord(letter) + key % 26
-		if nLC > 122:
-			nLC = nLC % 123 + 97
-		newLetter = chr(nLC)
-		letter = newLetter
-		newLetters.append(newLetter)
+    for letter in string:
+        nLC = ord(letter) + key % 26
+        if nLC > 122:
+            nLC = nLC % 123 + 97
+        newLetter = chr(nLC)
+        letter = newLetter
+        newLetters.append(newLetter)
 	
-	return "".join(newLetters)
+    return "".join(newLetters)
