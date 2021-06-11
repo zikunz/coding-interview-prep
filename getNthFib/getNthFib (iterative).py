952d59a24e8ee8e06927de8e8490cd2b7d33ef94
ZHU Zikun <75139323+zikunz@users.noreply.github.com>
2021-06-11 15:25:44 +0800

    Update and rename getNthFib.py (iterative).rtf to getNthFib (iterative).py

---
+++ getNthFib/getNthFib (iterative).py
@@ -0,0 +1,15 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+def getNthFib(n):
+    fib = 0
+    twoLatestNums = [0, 1]
+    count = 2
+	
+    while count < n:
+        nextFib = twoLatestNums[0] + twoLatestNums[1]
+        twoLatestNums[0] = twoLatestNums[1]
+        twoLatestNums[1] = nextFib
+        count += 1
+
+    return twoLatestNums[1] if n > 1 else twoLatestNums[0]
