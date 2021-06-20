b4a52c1458c00e916de0b0a559141a1dbffdbef8
ZHU Zikun <75139323+zikunz@users.noreply.github.com>
2021-06-20 19:09:14 +0800

    Update and rename findClosestValueInBst (recursive).py.rtf to findClosestValueInBst (recursive).py

---
+++ findClosestValueInBst/findClosestValueInBst (recursive).py
@@ -0,0 +1,26 @@
+# Time Complexity: O(N), where N represents number of nodes in the BST
+# Space Complexity: O(N), where N represents number of nodes in the BST (adding frames to the call stack due to recursive calls uses extra memory)
+
+def findClosestValueInBst(tree, target):
+    return findClosestValueInBstHelper(tree, target, float('inf'))
+
+def findClosestValueInBstHelper(tree, target, closest):
+    if tree is None:
+        return closest
+	
+    if abs(tree.value - target) < abs(closest - target):
+        closest = tree.value
+	
+    if target > tree.value:
+        return findClosestValueInBstHelper(tree.right, target, closest)
+    elif target < tree.value:
+        return findClosestValueInBstHelper(tree.left, target, closest)
+    else:
+        return closest
+	
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
